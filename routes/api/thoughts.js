4a34dc9d6025b4b63f4a845bd4788617929479ec
Joey O <myagentortega@gmail.com>
2023-05-21 09:44:44 -0600

    add method to create a new thought

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -1,19 +1,42 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
-getThoughts,
-getSingleThought,
-updateThought,
-deleteThought,
-createThought
-} = require('../../controllers/thoughtControllers');
+  getThoughts,
+  getSingleThought,
+  updateThought,
+  deleteThought,
+  createThought,
+} = require("../../controllers/thoughtControllers");
+const { Thought } = require("../../models");
 
+router.route("/").get(getThoughts).post(createThought);
 
-router.route('/').get(getThoughts).post(createThought);
+router
+  .route("/:thoughtsId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
 
-router.route("/:thoughtsId")
-.get(getSingleThought)
-.put(updateThought)
-.delete(deleteThought);
+router
+  .route("/:thoughtsId/reactions")
+  .post(async function (req, res) {
+    try {
+      // getting the reaction from the request body
+      const reaction = req.body;
 
+      // associate that reaction with the appropriate thought
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtsId },
+        { $push: { reactions: reaction } }
+      );
+
+      res.status(200).json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  })
+  .delete(async function (req, res) {
+    try {
+    } catch (error) {}
+  });
 
 module.exports = router;
